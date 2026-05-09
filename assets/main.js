dd478141b8e0b0b2ecd75667e2d7b1744e4c43a2
KevinR_XD69 <engs-kevinrichardt@camporeal.edu.br>
2026-05-09 01:15:57 -0300

    atividade 3 feita

--- assets/main.js
+++ assets/main.js
@@ -6,7 +6,7 @@ const add_start = document.querySelector("#add-start")
 const lista_telinhaUwU = document.querySelector("#lista_telinhaUwU")
 
 let lista_nomes = ["Forza", "Horizon", "Six"]
-
+// atividade 1
 function renderizarEmQuatroKa() {
 
     lista_telinhaUwU.innerHTML = ""
@@ -17,6 +17,23 @@ function renderizarEmQuatroKa() {
         lista_telinhaUwU.appendChild(item)
     }
 }
+//atividade 3
+
+function nomesGrandesDemaisSlk() {
+    let nomesGrandesDemaisSlk = 0
+
+    lista_nomes.forEach(nome => {
+        if (nome.length > 5){
+            nomesGrandesDemaisSlk++
+        }
+    })
+
+    nomesGrandesDemaisSlk.innerText = 
+        "Quantidade de palavras com mais de 5 letras: "
+        + nomesGrandesDemaisSlk
+}
+
+// atividade 2
 
 add_final.addEventListener("click", () => {
 
