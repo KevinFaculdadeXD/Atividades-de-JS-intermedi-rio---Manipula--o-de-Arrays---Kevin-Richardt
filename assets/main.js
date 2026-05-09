c50ffc2b93cdfc024a51f9f8d3ab0e177edc24c3
KevinR_XD69 <engs-kevinrichardt@camporeal.edu.br>
2026-05-09 00:27:33 -0300

    agora sim fiz a atividade 2 kkkkk

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,8 @@
 const input = document.getElementById("input")
 const add_final = document.querySelector("#add-final")
+const remove_final = document.querySelector("#remove-final")
+const remove_first = document.querySelector("#remove-first")
+const add_start = document.querySelector("#add-start")
 const lista_telinhaUwU = document.querySelector("#lista_telinhaUwU")
 
 let lista_nomes = ["Forza", "Horizon", "Six"]
@@ -19,6 +22,7 @@ add_final.addEventListener("click", () => {
 
     let valor_input = input.value
 
+    // evita adicionar vazio
     if (valor_input.trim() !== "") {
         lista_nomes.push(valor_input)
 
@@ -30,6 +34,35 @@ add_final.addEventListener("click", () => {
     }
 })
 
+remove_final.addEventListener("click", () => {
+
+    let valor_input = input.value
+        lista_nomes.pop(valor_input)
+        renderizarEmQuatroKa()
+    })
+
+remove_first.addEventListener("click", () => {
+
+    let valor_input = input.value
+        lista_nomes.shift(valor_input)
+        renderizarEmQuatroKa()
+    })
+
+add_start.addEventListener("click", () => {
+
+    let valor_input = input.value
+
+    // evita adicionar vazio
+    if (valor_input.trim() !== "") {
+        lista_nomes.unshift(valor_input)
+
+        console.log(lista_nomes)
+
+        renderizarEmQuatroKa()
+
+        input.value = ""
+    }
+})
 document.addEventListener("DOMContentLoaded", () => {
     renderizarEmQuatroKa()
 })
